a488fafc5b1ab308bdf9562b355bc6bb2f040bce
Dare6evil <dare6evil@gmail.com>
2025-03-19 14:35:02 +0900

    Add files via upload

--- PyTorch/Dueling Q-learning/train.py
+++ PyTorch/Dueling Q-learning/train.py
@@ -1,4 +1,3 @@
-import copy
 import gymnasium
 import numpy
 import os
@@ -7,55 +6,53 @@ import torch
 
 sys.path.append(os.pardir)
 import data
-import models
+import modules
 
+annealing_num_steps = 1000
 batch_size = 32
 buffer_size = 10000
-criterion = torch.nn.MSELoss()
-environment = gymnasium.make('LunarLander-v3', render_mode='human')
-episode = 300
-epsilon_decay = 0.995
-epsilon_end = 0.1
-epsilon_start = 1.0
-epsilon = epsilon_start
+env = gymnasium.make('LunarLander-v3', render_mode='human')
+eps_end = 0.1
+eps_init = 1.0
+eps = eps_init
 gamma = 0.9
-learning_rate = 0.01
-observation, _ = environment.reset()
-q_network = models.DuelingQNetwork(*environment.observation_space.shape, environment.action_space.n)
-optimizer = torch.optim.SGD(q_network.parameters(), learning_rate)
+lr = 0.01
+num_episodes = 300
+q = modules.DuelingQ(*env.observation_space.shape, env.action_space.n)
+optimizer = torch.optim.SGD(q.parameters(), lr)
 replay_buffer = data.ReplayBuffer(buffer_size)
+state, _ = env.reset()
 sync_interval = 20
-target_q_network = models.DuelingQNetwork(*environment.observation_space.shape, environment.action_space.n)
-for i in range(episode):
+target_q = modules.DuelingQ(*env.observation_space.shape, env.action_space.n)
+for i in range(num_episodes):
     while True:
-        if epsilon < numpy.random.rand():
-            action = q_network(torch.Tensor(observation)).argmax().item()
+        if eps < numpy.random.rand():
+            action = q(torch.Tensor(state)).argmax().item()
         else:
-            action = numpy.random.choice(environment.action_space.n)
-        next_observation, reward, terminated, truncated, _ = environment.step(action)
+            action = numpy.random.choice(env.action_space.n)
+        next_state, reward, terminated, truncated, _ = env.step(action)
         done = terminated or truncated
-        replay_buffer.add(observation, action, reward, next_observation, int(done),
-                          abs((1 - int(done)) * gamma * q_network(
-                              torch.Tensor(next_observation).detach()).max() + reward -
-                              q_network(torch.Tensor(observation))[action]))
+        replay_buffer.add(state, action, reward, next_state, int(done),
+                          abs((1 - int(done)) * gamma * q(torch.Tensor(next_state).detach()).max() + reward -
+                              q(torch.Tensor(state))[action]))
         if batch_size < len(replay_buffer):
-            observation_batch, action_batch, reward_batch, next_observation_batch, done_batch, _ = replay_buffer.sample(
-                batch_size)
-            loss = criterion((1 - torch.Tensor(numpy.array(done_batch))) * gamma *
-                             target_q_network(torch.Tensor(numpy.array(next_observation_batch)).detach())[
-                                 numpy.arange(batch_size), q_network(
-                                     torch.Tensor(numpy.array(observation_batch))).argmax(axis=1)] + torch.Tensor(
-                reward_batch), q_network(torch.Tensor(numpy.array(observation_batch)))[
-                                 numpy.arange(batch_size), action_batch])
+            state_batch, action_batch, reward_batch, next_state_batch, done_batch, _ = replay_buffer.sample(batch_size)
+            loss = torch.nn.functional.mse_loss((1 - torch.Tensor(numpy.array(done_batch))) * gamma *
+                                                target_q(torch.Tensor(numpy.array(next_state_batch)).detach())[
+                                                    numpy.arange(batch_size), q(
+                                                        torch.Tensor(numpy.array(state_batch))).argmax(
+                                                        dim=1)] + torch.Tensor(reward_batch),
+                                                q(torch.Tensor(numpy.array(state_batch)))[
+                                                    numpy.arange(batch_size), action_batch])
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
         if done:
-            observation, _ = environment.reset()
+            state, _ = env.reset()
             break
-        observation = next_observation
-    epsilon = max(epsilon * epsilon_decay, epsilon_end)
+        state = next_state
+    eps = max(eps - (eps_init - eps_end) / annealing_num_steps, eps_end)
     if not i % sync_interval:
-        target_q_network = copy.deepcopy(q_network)
-environment.close()
-torch.save(q_network.state_dict(), 'Dueling Q-learning.pth')
+        target_q.load_state_dict(q.state_dict())
+env.close()
+torch.save(q.state_dict(), 'Dueling Q-learning.pth')

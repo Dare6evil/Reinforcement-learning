04c54551107803f641b376fa1929908ec1900443
Dare6evil <dare6evil@gmail.com>
2025-03-22 17:18:01 +0900

    Add files via upload

---
+++ PyTorch/Double DQN/test.py
@@ -0,0 +1,20 @@
+import gymnasium
+import os
+import sys
+import torch
+
+sys.path.append(os.pardir)
+import modules
+
+env = gymnasium.make('LunarLander-v3', render_mode='human')
+q = modules.Q(*env.observation_space.shape, env.action_space.n)
+q.eval()
+q.load_state_dict(torch.load('Double DQN.pth', weights_only=True))
+state, _ = env.reset()
+while True:
+    action = q(torch.Tensor(state)).argmax().item()
+    next_state, _, terminated, truncated, _ = env.step(action)
+    if terminated or truncated:
+        break
+    state = next_state
+env.close()
--- PyTorch/modules.py
+++ PyTorch/modules.py
@@ -10,21 +10,3 @@ class Q(torch.nn.Module):
     def forward(self, x):
         q = torch.nn.functional.relu(self.layer1(x))
         return self.layer2(q)
-
-
-class DuelingQ(torch.nn.Module):
-    def __init__(self, in_features, out_features):
-        super().__init__()
-        self.layer1 = torch.nn.Linear(in_features, 100)
-        self.layer2 = torch.nn.Linear(100, 1)
-        self.layer3 = torch.nn.Linear(in_features, 100)
-        self.layer4 = torch.nn.Linear(100, out_features)
-
-    def forward(self, x):
-        v = torch.nn.functional.relu(self.layer1(x))
-        v = self.layer2(v)
-        a = torch.nn.functional.relu(self.layer3(x))
-        a = self.layer4(a)
-        if 1 < a.ndim:
-            return v + a - a.mean(dim=1, keepdim=True)
-        return v + a - a.mean()

8af12997755f3f1025808511687ff47b6ea12831
Dare6evil <dare6evil@gmail.com>
2025-04-13 17:34:45 +0900

    Add files via upload

--- PyTorch/Actor-Critic/train.py
+++ PyTorch/Actor-Critic/train.py
@@ -1,29 +1,30 @@
 from matplotlib import pyplot
-import gymnasium
-import numpy
 import os
 import sys
-import torch
 
 sys.path.append(os.pardir)
+import gymnasium
 import modules
+import numpy
+import torch
 
 device = torch.device("cpu" if not torch.cuda.is_available() else "cuda:0")
-env = gymnasium.make("CartPole-v1", render_mode="human")
-episodes = 3000
+env = gymnasium.make("CartPole-v0", render_mode="human")
+episodes = 300
 gamma = 0.98
 lr_pi = 0.0002
 lr_v = 0.0005
+m = 0
 reward_history = [0] * episodes
 runs = 100
 for run in range(1, 1 + runs):
     pi = modules.Policy(env.action_space.n, *env.observation_space.shape)
     pi.to(device)
     optimizer_pi = torch.optim.Adam(pi.parameters(), lr_pi)
-    state, _ = env.reset()
     v = modules.V(*env.observation_space.shape)
     v.to(device)
     optimizer_v = torch.optim.Adam(v.parameters(), lr_v)
+    state, _ = env.reset()
     for episode in range(episodes):
         total_reward = 0
         while True:
@@ -34,20 +35,24 @@ for run in range(1, 1 + runs):
             done = terminated or truncated
             target = (1 - done) * gamma * v(torch.Tensor(next_state).to(device).detach()) + reward
             loss_pi = -(target - b) * torch.log(probs[action])
+            loss_v = torch.nn.functional.mse_loss(target, b)
             optimizer_pi.zero_grad()
             loss_pi.backward(retain_graph=True)
-            loss_v = torch.nn.functional.mse_loss(target, b)
+            optimizer_pi.step()
             optimizer_v.zero_grad()
             loss_v.backward()
-            optimizer_pi.step()
             optimizer_v.step()
             total_reward += reward
             if done:
                 state, _ = env.reset()
                 break
             state = next_state
+        if m < total_reward:
+            m = total_reward
+            torch.save(pi.state_dict(), "Actor-Critic.pth")
         reward_history[episode] += (total_reward - reward_history[episode]) / run
-    # torch.save(pi.state_dict(), "Actor-Critic.pth")
 env.close()
 pyplot.plot(reward_history)
+pyplot.xlabel("Episode")
+pyplot.ylabel("Total Reward")
 pyplot.show()
